8cb7dad8108263f6702f16b4d9c976a756a4db7d
David Murray <david.murray@strath.ac.uk>
2017-08-23 12:58:10 +0100

    changed falling edge detection

--- EdgeDetect_Fall2.m
+++ EdgeDetect_Fall2.m
@@ -1,50 +1,71 @@
 function [fallingEdge] = EdgeDetect_Fall2 ...
     (Time, Data,rising_Edge, min_Pow, max_Pow, durationShort, durationLong)
-Data_Diff = diff(Data);
-Data_Diff_Offset1 = [Data_Diff(2:end); 0;];
-Data_Diff_Offset2 = [Data_Diff(3:end); 0;0;];
+D_Diff = diff(Data);
+D_Diff_Offset = [D_Diff(2:end); 0;];
+D_Diff_Offset2 = [D_Diff(3:end); 0;0;];
 
 % Sum of differences
-Data_Diff_Sum_1 = Data_Diff+Data_Diff_Offset1;
-Data_Diff_Sum_2 = Data_Diff+Data_Diff_Offset1+Data_Diff_Offset2;
-fallingEdge = NaN(length(rising_Edge),1);
-total = length(rising_Edge);
+Data_Diff_Sum_1 = D_Diff+D_Diff_Offset;
 
+D_Diff_Sum_1 = ([diff(Data);0] + [0;diff(Data)]);
+D_Diff_Sum_1(D_Diff_Sum_1 > 0) = 0;
+D_Diff_Sum_1_Off = [D_Diff_Sum_1(2:end);0;];
+
+D_Diff_Sum_2 = ([diff(Data);0;0;] + [0;diff(Data);0;] + [0;0;diff(Data);]);
+D_Diff_Sum_2(D_Diff_Sum_2 > 0) = 0;
+D_Diff_Sum_2_Off = [D_Diff_Sum_2(2:end);0;];
+
+%disp([size(D_Diff) size(D_Diff_Sum_1) size(D_Diff_Sum_2)])
+
+Data_Diff_Sum_2 = D_Diff+D_Diff_Offset+D_Diff_Offset2;
+total = length(rising_Edge);
+fallingEdge = NaN(total,1);
 TimeNum = posixtime(Time);
-disp(size(TimeNum))
+%disp(size(TimeNum))
 TimeLimit = Time(rising_Edge)+seconds(durationShort) < Time(end);
 for i =1:total
     if TimeLimit(i) == 1
-        rising_Edges_Sum = sum(Data_Diff(rising_Edge(i):rising_Edge(i)+2));
+        rising_Edges_Sum = sum(D_Diff(rising_Edge(i):rising_Edge(i)+2));
     else
         disp('Detected a bad edge')
         break
     end
     if rising_Edges_Sum > min_Pow && rising_Edges_Sum < max_Pow
-        beg = rising_Edge(i) + 1;
-        disp([i total])
-        %search_Time = Time > Time(beg+1) & Time < Time(beg+1)+seconds(durationLong);
-        search_Time = TimeNum > TimeNum(beg+1) & TimeNum < (TimeNum(beg+1)+durationLong);
-        search_Range = Data_Diff(search_Time,1);
-        search_Range_Off = Data_Diff_Offset1(search_Time,1);
+        beg = rising_Edge(i);
+        %disp([i total])
+        search_Time = TimeNum > TimeNum(beg) & TimeNum < (TimeNum(beg)+durationLong);
+        search_Range = D_Diff(search_Time,1);
+        search_Range_Off = D_Diff_Offset(search_Time,1);
         fall_E = find(search_Range > -max_Pow & search_Range_Off < -min_Pow &  search_Range_Off > -max_Pow ,1,'first');
-        if isempty(fall_E)
-            fall_E_S = find(Data_Diff_Sum_1(beg+5:beg+durationLong,1) < -min_Pow &  Data_Diff_Sum_1(beg+5:beg+durationLong,1) > -max_Pow ,1,'first');
-            if isempty(fall_E_S)
-                fall_E_S_2 = find(Data_Diff_Sum_2(beg+5:beg+durationLong,1) < -min_Pow &  Data_Diff_Sum_2(beg+5:beg+durationLong,1) > -max_Pow ,1,'first');
-                if isempty(fall_E_S_2)
-                    fallingEdge(i,1) = NaN;
-                else
-                    fallingEdge(i,1) = fall_E_S_2+beg+3;
-                end
-            else
-                fallingEdge(i,1) = fall_E_S+beg+3;
-            end
-        else
-            fallingEdge(i,1) = fall_E+beg+3;
-        end
-    else
-        fallingEdge(i,1) = NaN;
+        search_Range1 = D_Diff_Sum_1(search_Time,1);
+        search_Range_Off1 = D_Diff_Sum_1_Off(search_Time,1);
+        fall_E_1 = find(search_Range1 > -max_Pow & search_Range_Off1 < -min_Pow &  search_Range_Off1 > -max_Pow ,1,'first');
+        search_Range2 = D_Diff_Sum_2(search_Time,1);
+        search_Range_Off2 = D_Diff_Sum_2_Off(search_Time,1);
+        fall_E_2 = find(search_Range2 > -max_Pow & search_Range_Off2 < -min_Pow &  search_Range_Off2 > -max_Pow ,1,'first');
+        %disp('A')
+        fallingEdge = min(unique([fall_E; fall_E_1; fall_E_2]));
+        %make this unique
+        %if isempty(fall_E)
+            
+            %disp(size(search_Range))
+            %fall_E = 
+        %    fall_E_S = find(Data_Diff_Sum_1(beg+5:beg+durationLong,1) < -min_Pow &  Data_Diff_Sum_1(beg+5:beg+durationLong,1) > -max_Pow ,1,'first');
+        %    if isempty(fall_E_S)
+        %        fall_E_S_2 = find(Data_Diff_Sum_2(beg+5:beg+durationLong,1) < -min_Pow &  Data_Diff_Sum_2(beg+5:beg+durationLong,1) > -max_Pow ,1,'first');
+        %        if isempty(fall_E_S_2)
+        %            fallingEdge(i,1) = NaN;
+        %        else
+        %          fallingEdge(i,1) = fall_E_S_2+beg+3;
+        %        end
+         %   else
+         %       fallingEdge(i,1) = fall_E_S+beg+3;
+         %   end
+        %else
+        %    fallingEdge(i,1) = fall_E+beg+3;
+        %end
+    %else
+        %    fallingEdge(i,1) = NaN;
     end
 end
 end
